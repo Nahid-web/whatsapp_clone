6a9aeacc0b4897c29a484a88ce5078ac66e5e29d
Nahid-web <nahidamin266@gmail.com>
2024-12-20 16:05:18 +0600

    sent text message

---
+++ lib/features/chat/widgets/bottom_chat_field.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:whatsapp_clone/colors.dart';
+import 'package:whatsapp_clone/features/chat/controller/chat_controller.dart';
+
+class BottomChatField extends ConsumerStatefulWidget {
+  final String receiverUserId;
+  const BottomChatField(
+    this.receiverUserId, {
+    super.key,
+  });
+
+  @override
+  ConsumerState<BottomChatField> createState() => _BottomChatFieldState();
+}
+
+class _BottomChatFieldState extends ConsumerState<BottomChatField> {
+  bool isShowSendButton = false;
+  final TextEditingController _messageController = TextEditingController();
+
+  void sendTextMessage() {
+    if (isShowSendButton) {
+      ref.read(chatControllerProvider).sendTextMessage(
+            context,
+            _messageController.text.trim(),
+            widget.receiverUserId,
+          );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: TextFormField(
+            controller: _messageController,
+            onChanged: (value) {
+              if (value.isNotEmpty) {
+                setState(() {
+                  isShowSendButton = true;
+                });
+              } else {
+                setState(() {
+                  isShowSendButton = false;
+                });
+              }
+            },
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: mobileChatBoxColor,
+              prefixIcon: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: SizedBox(
+                  width: 100,
+                  child: Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.emoji_emotions,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.gif),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              suffixIcon: SizedBox(
+                width: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.camera_alt,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.attach_file,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              hintText: 'Type a message!',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20.0),
+                borderSide: const BorderSide(
+                  width: 0,
+                  style: BorderStyle.none,
+                ),
+              ),
+              contentPadding: const EdgeInsets.all(10),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(
+            bottom: 8,
+            right: 2,
+            left: 2,
+          ),
+          child: CircleAvatar(
+            child: IconButton(
+                onPressed: sendTextMessage,
+                icon: isShowSendButton
+                    ? Icon(Icons.send)
+                    : Icon(
+                        Icons.mic,
+                        color: Colors.white,
+                      )),
+          ),
+        )
+      ],
+    );
+  }
+}
