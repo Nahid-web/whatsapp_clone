600137c716c7c1a26b075a82e51b45ae914f7066
Nahid-web <nahidamin266@gmail.com>
2025-01-11 20:30:01 +0600

    remove unnecasry comment

--- lib/common/repositories/common_storage_repository.dart
+++ lib/common/repositories/common_storage_repository.dart
@@ -8,27 +8,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
 import 'package:whatsapp_clone/common/config/app_config.dart';
 
-/* import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final commonFirebaseStoreageRepository = Provider(
-  (ref) => CommonFirebaseStorageRepository(
-      firebaseStorage: FirebaseStorage.instance),
-);
-
-class CommonFirebaseStorageRepository {
-  final FirebaseStorage firebaseStorage;
-
-  CommonFirebaseStorageRepository({required this.firebaseStorage});
-
-  Future<String> storeFileToFirebase(String ref, File file) async {
-    UploadTask uploadTask = firebaseStorage.ref().child(ref).putFile(file);
-    TaskSnapshot snap = await uploadTask;
-    String downloadUrl = await snap.ref.getDownloadURL();
-    return downloadUrl;
-  }
-} */
-
 final commonCloudinryStorageRepository = Provider(
   (ref) => CommonCloudinryStorageRepository(
     cloudinary: CloudinaryObject.fromCloudName(cloudName: cloudName),
